a46d737cd6ba28524674fccc813bacc9e47c8e22
Rujuta Purohit <rujuta.purohit.24@dartmouth.edu>
2025-08-14 10:56:11 -0800

    triangle.py

---
+++ triangle.py
@@ -0,0 +1,21 @@
+def hypot(a: float, b: float):
+    """
+    Compute the hypotenuese of a right triangle
+
+    Args:
+        a (float): Length of one side next to hypot
+        b (float): Length of other side next to hypot
+
+    Returns:
+        float: Length of hypot
+
+    Examples:
+        hypot(3, 4) returns 5
+    """
+    # Check input types
+    if a < 0 or b < 0:
+        print("a and b must be positive")
+    if a == 0 or b == 0:
+        print("a and b cannot be zero")
+    else:
+        return (a**2 + b**2)**(0.5)
